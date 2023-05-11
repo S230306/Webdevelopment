d2314118581a6e357cf5c203b4c0f5fd1ef661d6
Shailendra <rajakshailendra123@gmail.com>
2023-05-11 13:19:09 +0530

    Web Development

---
+++ JAVASCRIPT/main1.js
@@ -0,0 +1,4 @@
+document.write("5+6=11");
+window.alert("See the output here");
+console.log("Hey meta can u give me a job.");
+window.print("Print");
---
+++ JAVASCRIPT/main2.js
@@ -0,0 +1,7 @@
+let a, b, c;
+a = 10;
+b = 18;
+c = a + b;
+function myfunction(){
+ document.getElementById('demo').innerHTML=c;
+}
